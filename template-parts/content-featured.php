b39cdc82a03afc2c4ba7bd878450af5c322135ad
KienTrong <kienpc81089@gmail.com>
2016-08-23 09:26:43 +0700

    Remove empty line, testing ST3 gitsavvy

--- template-parts/content-featured.php
+++ template-parts/content-featured.php
@@ -24,5 +24,4 @@
 		} ?>
 		<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
 	</header><!-- .entry-header -->
-
 </article><!-- #post-## -->
